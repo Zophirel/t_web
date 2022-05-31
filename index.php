0426b59708e8690410086e53ccef92f8c2a290cb
Francesco <sworld.design@gmail.com>
2022-05-31 12:01:22 +0200

    homepage fatta

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="assets/css/style.css">
+    </head>
+    <body>
+        <div id="homePageLayout">
+            <p class="slogan">Post your <br> image and  <br> start earning  <br>from it</p>
+            <div class="loginForm">
+                <div id="loginInputs">                
+                    <h1 id="loginHeader">Sign in</h1>
+                    
+                    <label class="loginLabel">email</label>
+                    <input class="loginField" type="text">          
+                    <label class="loginLabel">password</label>
+                    <input class="loginField" type="text">
+                    
+                
+                    <button id="formBtn"><p>Sign in<p></button>
+                    <p>Don't have an account yet?</p>
+                    <p><a href="/signup.php">join us</a></p>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
