3334f84d4f290453c6c4221dc3fde085daa9ae0a
fra <noreply@zophirel.it>
2022-06-01 22:08:44 +0200

    added bootstrap

--- index.php
+++ index.php
@@ -1,26 +1,23 @@
-<html>
+<html> 
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="assets/css/style.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="assets/css/style.css">        
     </head>
+
     <body>
-        <div id="homePageLayout">
-            <p class="slogan">Post your <br> image and  <br> start earning  <br>from it</p>
-            <div class="loginForm">
-                <div id="loginInputs">                
-                    <h1 id="loginHeader">Sign in</h1>
-                    
-                    <label class="loginLabel">email</label>
-                    <input class="loginField" type="text">          
-                    <label class="loginLabel">password</label>
-                    <input class="loginField" type="text">
-                    
-                
-                    <button id="formBtn"><p>Sign in<p></button>
-                    <p>Don't have an account yet?</p>
-                    <p><a href="/signup.php">join us</a></p>
+            
+            <div class="row m-0 h-100 w-100 align-content-center">
+                <div class="col">
+                    <div style="width: 325px; margin: 0 auto;">
+                        <p class="slogan">Post your <br> image and  <br> start earning  <br>from it</p>
+                    </div>
                 </div>
+                
+                <div class="col p-0"><div class="card" style="height: 450px; width: 350px; margin: 0 auto;"></div></div>
             </div>
-        </div>
+       
     </body>
+    
 </html>
